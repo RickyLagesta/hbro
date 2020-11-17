95be69e5291fb77488739925d73e262786ffc326
RickyLagesta <74608885+RickyLagesta@users.noreply.github.com>
2020-11-17 15:41:03 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`tamo redi como ${client.user.tag}!`);
+  });
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }
+  });
+
+client.login('Nzc4MjQzNjQ2ODcyMjIzNzc2.X7PJ4A.p8NmpPaSkmowEDA-kQdcpPbn2VE');
