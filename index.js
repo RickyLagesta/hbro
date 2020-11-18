e5a1f17c2abd50763a0275ec665e4d31e99189cd
RickyLagesta <74608885+RickyLagesta@users.noreply.github.com>
2020-11-18 02:15:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,130 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+const ytdl = require("ytdl-core");
+
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`tamo redi como ${client.user.tag}!`);
-  });
+const queue = new Map();
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
+client.once("ready", () => {
+  console.log("tamo redi!");
+});
+
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
+});
+
+client.once("disconnect", () => {
+  console.log("Disconnect!");
+});
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const serverQueue = queue.get(message.guild.id);
+
+  if (message.content.startsWith(`${prefix}play`)) {
+    execute(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}skip`)) {
+    skip(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}stop`)) {
+    stop(message, serverQueue);
+    return;
+  } else {
+    message.channel.send("dilo bien o me enfado");
+  }
+});
+
+async function execute(message, serverQueue) {
+  const args = message.content.split(" ");
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "You need to be in a voice channel to play music!"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "I need the permissions to join and speak in your voice channel!"
+    );
+  }
+
+  const songInfo = await ytdl.getInfo(args[1]);
+  const song = {
+        title: songInfo.videoDetails.title,
+        url: songInfo.videoDetails.video_url,
+   };
+
+  if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+
+    queue.set(message.guild.id, queueContruct);
+
+    queueContruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
     }
-  });
+  } else {
+    serverQueue.songs.push(song);
+    return message.channel.send(`${song.title} ya verá que temazo puso este a la cola`);
+  }
+}
 
-client.login('Nzc4MjQzNjQ2ODcyMjIzNzc2.X7PJ4A.G0aDjFOmMuDRZ2eiEmhn8Dht9wU');
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "no me grite que no te cuxo"
+    );
+  if (!serverQueue)
+    return message.reply("pero que dices payaso si no hay");
+  serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "illo que te tiene que poné nun caná pa zoná"
+    );
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+  message.channel.send("vale ya paro");
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("error", error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 10);
+  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+}
+
+client.login(token);
